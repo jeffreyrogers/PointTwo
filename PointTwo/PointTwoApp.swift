db6fb23af60f053e77d9d3707225926443bfecab
Jeff Rogers <jeffreyrogers27@gmail.com>
2021-06-07 12:28:45 -0600

    Initial Commit

---
+++ PointTwo/PointTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PointTwoApp.swift
+//  PointTwo
+//
+//  Created by Jeffrey Rogers on 6/7/21.
+//
+
+import SwiftUI
+
+@main
+struct PointTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
