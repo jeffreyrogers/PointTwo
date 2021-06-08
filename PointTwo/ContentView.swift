4e92e044ffbc758e140a0fc6c540b6dd81254362
Jeff Rogers <jeffreyrogers27@gmail.com>
2021-06-07 19:32:43 -0600

    Get basic functionality completed
    Can now set two points and calculate the distance between them. Typography could be improved.
    Distance should maybe update automatically rather than needing to be recalculated manually.

--- PointTwo/ContentView.swift
+++ PointTwo/ContentView.swift
@@ -10,35 +10,56 @@ import CoreLocation
 
 struct ContentView: View {
     @ObservedObject var LVM = LocationViewModel()
+    @State private var point1 = CLLocation(latitude: 0, longitude: 0)
+    @State private var point2 = CLLocation(latitude: 0, longitude: 0)
+    @State private var distance = 0.0
     
     var body: some View {
         VStack() {
-            Text("Your Location Is")
+            Group {
+                Spacer()
+                Text("Current Location")
+                Text("Lat: \(LVM.userLocation.coordinate.latitude)")
+                Text("Long: \(LVM.userLocation.coordinate.longitude)")
+                Text("Alt: \(LVM.userLocation.altitude)")
+                Spacer()
+            }
+
+            Divider()
+
             HStack {
-                Text("Latitude: \(LVM.userLocation.coordinate.latitude)")
-                Text("Longitude: \(LVM.userLocation.coordinate.longitude)")
-                Text("Altitude: \(LVM.userLocation.altitude)")
+                VStack {
+                    Text("Point 1")
+                    Text("Lat: \(point1.coordinate.latitude)")
+                    Text("Long: \(point1.coordinate.longitude)")
+                    Text("Alt: \(point1.altitude)")
+                    Button("Store Point 1") {
+                        point1 = LVM.userLocation
+                    }
+                }
+
+                VStack {
+                    Text("Point 2")
+                    Text("Lat: \(point2.coordinate.latitude)")
+                    Text("Long: \(point2.coordinate.longitude)")
+                    Text("Alt: \(point2.altitude)")
+                    Button("Store Point 2") {
+                        point2 = LVM.userLocation
+                    }
+                }
+            }
+
+            Divider()
+
+            Group {
+                Spacer()
+                Text("\(distance) meters from point 1 to point 2")
+                Button("Calculate Distance") {
+                    distance = point1.distance(from: point2)
+                }
+                Spacer()
             }
         }
-//        VStack() {
-//            Text("Point 1")
-//                .padding()
-//            Button("Set Point 1") {
-//
-//            }
-//            Spacer()
-//            Text("Point 2")
-//                .padding()
-//            Button("Set Point 2") {
-//
-//            }
-//            Spacer()
-//            Divider()
-//            Text("Distance (m)")
-//                .padding()
-//            Text("")
-//                .padding()
-//        }
     }
 }
 
