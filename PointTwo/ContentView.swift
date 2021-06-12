1fe1fb12370e0fa786a307bdd8d468ce76e9d36c
Jeff Rogers <jeffreyrogers27@gmail.com>
2021-06-12 13:50:07 -0600

    Use SceneStorage instead of CoreData to save location info.
    Required subclassing CLLocation to make this work because @SceneStorage can only store RawRepresentable types.
    CLLocation is not RawRepresentable, but it is codable, so I was able to use NSKeyedArchiver to implement the
    RawRepresentable protocol on my subclass.
    
    Because RawRepresentable requires implementing an initializer, it was not possible to simply extend CLLocation with the protocol.

--- PointTwo/ContentView.swift
+++ PointTwo/ContentView.swift
@@ -8,12 +8,50 @@
 import SwiftUI
 import CoreLocation
 
+class P2Location: CLLocation, RawRepresentable {
+    init(location: CLLocation) {
+        super.init(coordinate: location.coordinate, altitude: location.altitude, horizontalAccuracy: location.horizontalAccuracy, verticalAccuracy: location.verticalAccuracy, timestamp: location.timestamp)
+    }
+    
+    override convenience init() {
+        self.init(location: CLLocation(latitude: 0, longitude: 0))
+    }
+    
+    required convenience init(rawValue: String) {
+        guard let data = Data(base64Encoded: rawValue) else {
+            self.init()
+            return
+        }
+        
+        do {
+            let loc = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? P2Location ?? P2Location()
+            self.init(location: loc)
+        } catch {
+            self.init()
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    public var rawValue: String {
+        do {
+            let data = try NSKeyedArchiver.archivedData(withRootObject: self, requiringSecureCoding: false) as Data
+            return data.base64EncodedString()
+        } catch {
+            return ""
+        }
+    }
+    
+}
+
 struct ContentView: View {
     @ObservedObject var LVM = LocationViewModel()
-    @State internal var point1 = CLLocation(latitude: 0, longitude: 0)
-    @State internal var point2 = CLLocation(latitude: 0, longitude: 0)
+    @SceneStorage("point1") var point1 = P2Location()
+    @SceneStorage("point2") var point2 = P2Location()
     @State private var distance = 0.0
-    
+
     var body: some View {
         VStack() {
             Group {
@@ -33,7 +71,7 @@ struct ContentView: View {
                         .padding()
                     LocationView(point: point1)
                     Button("Store Point 1") {
-                        point1 = LVM.userLocation
+                        point1 = P2Location(location: LVM.userLocation)
                         distance = point2.distance(from: point1)
                     }
                     .padding()
@@ -44,7 +82,7 @@ struct ContentView: View {
                         .padding()
                     LocationView(point: point2)
                     Button("Store Point 2") {
-                        point2 = LVM.userLocation
+                        point2 = P2Location(location: LVM.userLocation)
                         distance = point1.distance(from: point2)
                     }
                     .padding()
